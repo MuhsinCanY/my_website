e0f6bf913388820aa7b2d563973f58dcc88f16b8
MuhsinCanY <mcyilmaz1998@gmail.com>
2024-02-02 00:53:23 +0300

    skills dark-light complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className="w-screen h-[400px] flex">
-      <div className="bg-p_blue basis-3/4 grow flex justify-end">
+    <div className="w-screen h-[500px] flex">
+      <div className="bg-p_blue dark:bg-[#171043] basis-3/4 grow flex justify-end">
         <a
           href="#"
           className="m-5 font-[Inter] font-bold text-p_green hover:text-[#87b03c]"
         >
-          <span className="font-[Inter]">TÜRKÇE</span>
-          <span className="text-[#D9D9D9]">’YE GEÇ</span>
+          <span className="font-[Inter] dark:text-[#BAB2E7]">TÜRKÇE</span>
+          <span className="text-[#D9D9D9] dark:text-[#777777]">’YE GEÇ</span>
         </a>
       </div>
-      <div className="bg-p_green basis-1/4 grow flex justify-start">
+      <div className="bg-p_green dark:bg-[#1A210B] basis-1/4 grow flex justify-start">
         <a
           href="#"
           className="m-5 font-[Inter] font-bold text-p_blue hover:text-[#37418a]"
         >
-          <span className="font-[Inter]">DARK MODE</span>
+          <span className="font-[Inter] dark:text-[#D9D9D9]">DARK MODE</span>
         </a>
       </div>
     </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,9 +11,11 @@ const skills = [
 
 export default function Skills() {
   return (
-    <div className="main bg-slate-50 flex items-center justify-center">
+    <div className="main bg-slate-50 flex items-center justify-center dark:bg-[#252128]">
       <div className="container flex gap-10 p-16">
-        <h2 className="text-p_blue font-[Inter] font-bold text-5xl">Skills</h2>
+        <h2 className="text-p_blue dark:text-p_green font-[Inter] font-bold text-5xl">
+          Skills
+        </h2>
         <div className="skills flex flex-wrap gap-4 justify-evenly items-center">
           {skills.map((skill, i) => {
             return <SkillBox key={i} src={skill.img} name={skill.name} />
@@ -26,9 +28,11 @@ export default function Skills() {
 
 export function SkillBox({ src, name }) {
   return (
-    <div className="flex items-center gap-4 w-[240px]">
-      <img src={src} className="w-[120px] h-[120px] object-cover rounded-lg" />
-      <h3 className="text-[#777777] font-[Inter] font-medium">{name}</h3>
+    <div className="flex items-center gap-4 p-2 w-[240px]">
+      <img src={src} className="w-[110px] h-[110px] object-cover rounded-lg" />
+      <h3 className="text-[#777777] font-[Inter] font-medium dark:text-[#FFFFFF] ">
+        {name}
+      </h3>
     </div>
   )
 }
